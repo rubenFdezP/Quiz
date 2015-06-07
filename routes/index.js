638d50f54cefb58255c1185c3823e8cb5f2c431e
rubenFdezP <el_xin_13@hotmail.com>
2015-06-07 19:46:09 +0200

    Enlace a Git y Créditos

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,14 @@ var quizController = require('../controllers/quiz_controller')
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
+//GET /author
+router.get('/author', function(req, res) {
+	res.render('author', { title: 'Teo' });
+});
+
 
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer',   quizController.answer);
+
 
 module.exports = router;
