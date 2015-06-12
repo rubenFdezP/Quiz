c251b49adc138903cc9c353c3ac33777e1a0a349
rubenFdezP <el_xin_13@hotmail.com>
2015-06-12 18:04:01 +0200

    Autoload

--- models/models.js
+++ models/models.js
@@ -31,14 +31,17 @@ var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 exports.Quiz = Quiz;
 
 //sequelize.sync() crea e inicializa tabla de preguntas en DB
-sequelize.sync().success(function() {
-	//success(..) ejecuta el manejador unavez creada la tabla
-	Quiz.count().success(function (count){
+sequelize.sync().then(function() {
+	//then(..) ejecuta el manejador unavez creada la tabla
+	Quiz.count().then(function (count){
 		if (count === 0) {		//la tabla se inicializa solo si está vacía
 			Quiz.create({ pregunta: 'Capital de Italia',
 						  respuesta: 'Roma'
+						});
+			Quiz.create({ pregunta: 'Capital de Portugal',
+						  resùesta: 'Lisboa'
 						})
-			.success(function(){console.log('Base de datos inicializa')});
+			.then(function(){console.log('Base de datos inicializa')});
 		};
 	});
 });							  
