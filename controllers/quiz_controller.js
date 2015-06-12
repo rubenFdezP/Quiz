61880cb90beffc219f8f23a29cf7f1f62b952f76
rubenFdezP <el_xin_13@hotmail.com>
2015-06-12 20:06:19 +0200

    Busqueda

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,9 +14,17 @@ exports.load = function(req,res,next,quizId) {
 
 //GET /quizes
 exports.index = function(req,res) {
-	models.Quiz.findAll().then(function(quizes) {
+	if (req.query.search) {
+		models.Quiz.findAll({where:["pregunta like ?",'%'+req.query.search.replace(" ","%")+'%']}).then(function(quizes) {
 		res.render('quizes/index', {quizes: quizes});
-	})
+		}).catch(function(error) { next(error);})
+	} else {
+		models.Quiz.findAll().then(function(quizes) {
+		res.render('quizes/index', {quizes: quizes});
+		}
+	).catch(function(error) { next(error);});
+	}
+
 };
 
 //GET /quizes/:id
